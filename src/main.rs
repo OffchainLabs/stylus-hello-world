dbf050454e135d5ed952b8d718c33a12a437eaca
Raul Jordan <raul@prysmaticlabs.com>
2023-08-25 10:42:35 -0700

    first commit

---
+++ src/main.rs
@@ -0,0 +1,59 @@
+// Copyright 2023, Offchain Labs, Inc.
+// For license information, see https://github.com/OffchainLabs/nitro/blob/master/LICENSE
+// Warning: this code is a template only and has not been audited.
+
+//! Implements a hello-world example for Arbitrum Stylus, providing a Solidity ABI-equivalent
+//! Rust implementation of the Counter contract example provided by Foundry.
+//! ```
+//! contract Counter {
+//!     uint256 public number;
+//!     function setNumber(uint256 newNumber) public {
+//!         number = newNumber;
+//!     }
+//!     function increment() public {
+//!         number++;
+//!     }
+//! }
+//! ```
+
+// Only run this as a WASM if the export-abi feature is not set.
+#![cfg_attr(not(feature = "export-abi"), no_main)]
+
+/// Import the Stylus SDK along with alloy primitive types for use in our program.
+use stylus_sdk::{
+    alloy_primitives::{Uint, U256},
+    prelude::*,
+};
+
+/// Initializes a custom, global allocator for Rust programs compiled to WASM.
+#[global_allocator]
+static ALLOC: wee_alloc::WeeAlloc = wee_alloc::WeeAlloc::INIT;
+
+// Define the entrypoint as a Solidity storage object, in this case a struct
+// called `Counter` with a single uint256 value called `number`. The sol_storage! macro
+// will generate Rust-equivalent structs with all fields mapped to Solidity-equivalent
+// storage slots and types.
+sol_storage! {
+    #[derive(Entrypoint)]
+    struct Counter {
+        uint256 number;
+    }
+}
+
+/// Define an implementation of the generated Counter struct, defining a set_number
+/// and increment method using the features of the Stylus SDK.
+#[external]
+impl Counter {
+    /// Sets a number in storage to a user-specified value.
+    pub fn set_number(&mut self, new_number: U256) -> Result<(), Vec<u8>> {
+        self.number.set(new_number);
+        Ok(())
+    }
+
+    /// Increments number and updates it values in storage.
+    pub fn increment(&mut self) -> Result<(), Vec<u8>> {
+        let number = self.number.get();
+        self.number.set(number + Uint::from(1));
+        Ok(())
+    }
+}
