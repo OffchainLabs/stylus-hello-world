3d108c8e7d5889f3c3ea7f9ab008add4fabac718
Raul Jordan <raul@prysmaticlabs.com>
2023-08-28 15:04:47 -0700

    do not double

--- src/main.rs
+++ src/main.rs
@@ -15,6 +15,7 @@
 
 // Only run this as a WASM if the export-abi feature is not set.
 #![cfg_attr(not(feature = "export-abi"), no_main)]
+extern crate alloc;
 
 /// Import the Stylus SDK along with alloy primitive types for use in our program.
 use stylus_sdk::{alloy_primitives::U256, prelude::*};
