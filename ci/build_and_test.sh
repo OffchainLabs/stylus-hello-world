412063f77c20f73f59100e694cc5d9129983d0d4
Raul Jordan <raul@prysmaticlabs.com>
2024-02-01 13:50:30 -0600

    ci

--- ci/build_and_test.sh
+++ ci/build_and_test.sh
@@ -10,8 +10,4 @@ rustc -Vv
 cargo -V
 
 # Build and test main crate
-if [ "$CFG_RELEASE_CHANNEL" == "nightly" ]; then
-    cargo build --locked --all-features
-else
-    cargo build --locked
-fi
+cargo build --locked --all-features
