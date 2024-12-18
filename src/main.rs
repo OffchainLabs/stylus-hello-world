d58fbef43ed8b7379780f24c3b687e34490cb3a8
0xNeshi <nenad.misic@openzeppelin.com>
2024-12-18 10:40:00 +0100

    add main stub for builds

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,9 @@
 #![cfg_attr(not(feature = "export-abi"), no_main)]
 
+#[cfg(not(feature = "export-abi"))]
+#[no_mangle]
+pub extern "C" fn main() {}
+
 #[cfg(feature = "export-abi")]
 fn main() {
     stylus_hello_world::print_abi("MIT-OR-APACHE-2.0", "pragma solidity ^0.8.23;");
